5a437d4ba4785f5bfc9f62ebcaed5f643cd043ae
akhilesh <akhileshacademics@gmail.com>
2025-09-06 03:34:03 +0530

    Time: 3 ms (90.54%), Space: 21 MB (96.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,38 +1,11 @@
+import heapq
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d={}
         for i in nums:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
+            d[i] = d.get(i, 0) + 1
         return list(dict(sorted(d.items(),key=lambda x:x [1],reverse=True)))[:k]
-      #  return list(sorteditems)[:k]
-#TC 90.54 MS (LC) ,SC 99.30 MS (LC
-'''
- d={}
-        for i in nums:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-        sorteditems=sorted(d.items(),key=lambda x:x [1],reverse=True)
-        return list(dict(sorteditems[:k]))
-'''
-'''
-d={}
-        for i in nums:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-        sorteditems=dict(sorted(d.items(),key=lambda x:x [1],reverse=True))
-        return list(sorteditems)[:k]
-
-'''
 
 
-        
-        
-
-        
