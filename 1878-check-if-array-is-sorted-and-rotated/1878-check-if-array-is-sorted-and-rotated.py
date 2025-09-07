f4fd3b6a0d24d521c5637f27bd075578b747f22a
akhilesh <akhileshacademics@gmail.com>
2025-09-07 23:04:55 +0530

    Time: 0 ms (100%), Space: 17.8 MB (61.24%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -11,6 +11,15 @@ class Solution:
         return count <= 1
 
          
+'''
+count = 0
+n = len(nums)
+for i in range(n):
+    if nums[i] > nums[(i+1) % n]:
+        count += 1
+return count <= 1
 
+
+'''
 
                 
