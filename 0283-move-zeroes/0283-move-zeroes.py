628316157c8b8f4f6870e87c6739da895dc3bb23
akhilesh <akhileshacademics@gmail.com>
2025-09-09 01:34:28 +0530

    Time: 8 ms (26.9%), Space: 18.7 MB (95.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in nums[:]:
-            if i==0:
-                nums.append(0)
-                nums.remove(0)
+        #sliding window
+        j=0
+        for i in range(len(nums)):
+
+            if nums[i]!=0 and nums[j]==0:
+                nums[j],nums[i]=nums[i],nums[j]
+                
+            if nums[j]!=0:
+                j+=1
+        return nums
+            
