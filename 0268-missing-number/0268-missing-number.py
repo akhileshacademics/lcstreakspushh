29e5dbc9bcef6b75bb44238a9160e6fb0ce1e3a4
akhilesh <akhileshacademics@gmail.com>
2025-09-13 00:52:15 +0530

    Time: 8 ms (29.37%), Space: 19 MB (11%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
-        for i in range(len(nums)+1):
-            if i not in nums:
+        n = len(nums)
+        freq = {}   # dictionary as hash table
+
+    # Store frequencies of each number in nums
+        for num in nums:
+            freq[num] = 1   # or freq[num] = freq.get(num, 0) + 1
+
+        for i in range(n+1):
+            if i not in freq:
                return i
             
 
