e7e3884f8c0db276144a9a2e30e767d1c7865647
akhilesh <akhileshacademics@gmail.com>
2025-10-01 02:50:19 +0530

    Time: 0 ms (100%), Space: 19.5 MB (18.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count=0
-        majority=0
-        for i in range(len(nums)):
-            if count==0:
-                count=1
-                majority=nums[i]
-            elif nums[i]==majority:
-                count+=1
-            else :
-                count-=1
-        count1=0
-        for i in range(len(nums)):
-            if nums[i]==majority:
-                count1+=1
-        if count1>len(nums)/2:
-            return majority
+        #if majority element is not guarentied
+        # count=0
+        # majority=0
+        # for i in range(len(nums)):
+        #     if count==0:
+        #         count=1
+        #         majority=nums[i]
+        #     elif nums[i]==majority:
+        #         count+=1
+        #     else :
+        #         count-=1
+        # count1=0
+        # for i in range(len(nums)):
+        #     if nums[i]==majority:
+        #         count1+=1
+        # if count1>len(nums)/2:
+        #     return majority
+
+        
+        # if majority is guaranteed
+        count = 0
+        majority = 0
+        for num in nums:
+            if count == 0:
+                majority = num
+                count = 1
+            elif num == majority:
+                count += 1
+            else:
+                count -= 1
+
+        return majority   # if majority is guaranteed
+
 
