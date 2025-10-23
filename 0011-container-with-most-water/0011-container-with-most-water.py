6e9d278b2d22c14bfe0e70a72cc32a313de83f51
akhilesh <akhileshacademics@gmail.com>
2025-10-23 23:16:25 +0530

    Time: 15 ms (98.56%), Space: 28.5 MB (67.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,12 +6,17 @@ class Solution:
 
         while l<r:
             width = (r - l)
-            cur_area = width * min(height[r],height[l])
+            newheight=min(height[r],height[l])
+            cur_area = width * newheight
             maxarea=max(cur_area,maxarea)
             if height[l]<height[r]:
                 l+=1
+                while l<r and newheight>=height[l]:
+                    l+=1
             else :
                 r-=1
+                while l<r and newheight>=height[r]:
+                    r-=1
         return maxarea
             
 
