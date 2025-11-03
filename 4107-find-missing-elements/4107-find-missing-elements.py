201b0b46b19ac44c9b72760ffdafbbc02e0d32f8
akhilesh <akhileshacademics@gmail.com>
2025-11-03 19:49:50 +0530

    Time: 0 ms (100%), Space: 17.8 MB (68.5%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.py
+++ 4107-find-missing-elements/4107-find-missing-elements.py
@@ -5,10 +5,11 @@ class Solution:
         rangestart=min(nums)
         rangeend=max(nums)
         keep=[]
+        numset=set(nums)
         j=0
         for i in range(rangestart,rangeend):
 
-            if i not in set(nums):
+            if i not in numset:
                 keep.append(i)
             j+=1
 
