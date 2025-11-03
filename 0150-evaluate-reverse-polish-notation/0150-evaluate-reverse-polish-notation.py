67779a2675e867864783bb3a444dc91fc5c7bff9
akhilesh <akhileshacademics@gmail.com>
2025-11-03 18:43:30 +0530

    Time: 3 ms (64.12%), Space: 19.2 MB (16.64%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -3,23 +3,21 @@ class Solution:
         stack=[]
 
         for i in tokens:
+
             if i not in "*/-+":
                 stack.append(int(i))
+
             else:
                 val1=stack.pop()
                 val2=stack.pop()
 
             if i == "+":
                 stack.append(val1+val2)
-
             elif i == "-":
                 stack.append(val2-val1)
-
             elif i == "/":
                 stack.append(int(val2/val1))
-
             elif i == "*":
-
                 stack.append(val2*val1)
             
         return stack[-1]
